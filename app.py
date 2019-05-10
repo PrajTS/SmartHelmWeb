da7ea72140cf3d2d32d76d3fafabbc9f1655382e
Prajwal T S <tsprajwal@gmail.com>
2019-05-10 18:28:54 +0530

    removed sending hospital id

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route("/hospital/<string:hospitalid>")
 def viewAccidents(hospitalId):
-	return render_template('viewAccidents.html',hospitalid=hospitalid)
+	return render_template('viewAccidents.html')
 
 @app.route("/hospital/<string:hospitalid>/<string:userid>")
 def viewUserAccidentInfo(hospitalid, userid):
 	return render_template(viewUserAccidentInfo.html, hospitalid = hospitalid, userid = userid)
 
-@app.route("/<string:userid>")
+@app.route("emc/<string:userid>")
 def emC(userid):
 	return render_template('index.html',userid=userid)
 
